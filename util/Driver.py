7b61894217c4d0f3a55ba91da041b03729f976a3
Ghost6446 <62809003+Ghost6446@users.noreply.github.com>
2023-09-12 17:44:26 +0200

    Update Driver.py

--- util/Driver.py
+++ util/Driver.py
@@ -1,13 +1,13 @@
-# 5.07.2023
+# 5.07.2023 -> 12.09.2023
 
 # General import
-import os, time, ast, json, subprocess
+import os, time, subprocess
+from sys import platform
 from bs4 import BeautifulSoup
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 class Driver:
 
@@ -26,12 +26,9 @@ class Driver:
             except: pass
 
     def create(self, headless = False):
-        if(headless):
-            self.options.add_argument("headless")
-
+        if(headless): self.options.add_argument("headless")
         self.options.add_argument("--window-size=1280,1280")
         self.options.add_argument('--user-data-dir=C:/Users/'+os.getlogin()+'/AppData/Local/Google/Chrome/User Data')
-
         self.options.add_experimental_option("useAutomationExtension", True)
         self.options.add_experimental_option("excludeSwitches",["enable-automation"])
         self.options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -41,7 +38,6 @@ class Driver:
         self.options.add_argument("--allow-running-insecure-content")
         self.options.add_argument("--disable-notifications")
 
-        # Create drover
         self.driver = webdriver.Chrome(options=self.options,  service=self.service)
 
     def page_has_loaded(self):
@@ -52,30 +48,10 @@ class Driver:
         start_time = time.time()
         self.driver.get(url)
         time.sleep(sleep)
-        while( self.page_has_loaded() == False ):
-            time.sleep(sleep)
-        if show_url:
-            print("GET URL => ", url, " IN", time.strftime("%H:%M:%S",time.gmtime(time.time() - start_time) ))
-
-    def page_is_loading(self):
-        while True:
-            x = self.driver.execute_script("return document.readyState")
-            if x == "complete":
-                return True
-            else:
-                yield False
-
-    def scroll_page(self, sleep=1):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep)
+        while(self.page_has_loaded() == False): time.sleep(sleep)
 
     def get_soup(self):
         return BeautifulSoup(self.driver.page_source, "lxml")
-
-    def donwload_image(self, url, path):
-        wait = WebDriverWait(self.driver, 3)
-        self.driver.get(url)
-        self.driver.save_screenshot(path)
 
     def close(self): 
         print("Close driver")
